7e1556b4403fef49625636268201407a8a047de9
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-09-19 15:09:23 -0400

    added the conditional if statements

---
+++ exercise2.rb
@@ -0,0 +1,28 @@
+documentary = "Icarus"
+drama = "Gone With the Wind"
+comedy = "21 Jump Street"
+book = "LOTR"
+
+puts "On Scale of 1-5 Rate how much you like documentary"
+
+documentary_rating = gets.chomp
+
+puts "On Scale of 1-5 Rate how much you like comedy"
+
+comedy_rating = gets.chomp
+
+puts "On Scale of 1-5 Rate how much you like drama"
+
+drama_rating = gets.chomp
+
+if documentary_rating >= 4
+
+elsif comedy_rating >= 4 && drama_rating >= 4
+
+elsif comedy_rating >= 4
+
+elsif drama_rating >= 4
+
+else  
+
+end
