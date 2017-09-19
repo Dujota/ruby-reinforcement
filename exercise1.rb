7a2e423d34616f9150923a50e90746c889b68b82
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-09-19 14:32:51 -0400

    added all the conditional options that could posssibly happen

--- exercise1.rb
+++ exercise1.rb
@@ -1,26 +1,42 @@
   documenatary = "Icarus"
   drama = "Gone With the Wind"
   comedy = "21 Jump Street"
+  book = "LOTR"
+
+  puts "Do you like documenatary? \n yes or no?"
+  documenatary-choice = gets.chomp
+
+  puts "Do you like comedy? \n yes or no?"
+  comedy-choice = gets.chomp
+
+  puts "Do you like drama? \n yes or no?"
+  drama-choice = gets.chomp
 
 
 
-  puts "Do you like documenataries? \n yes or no is valid input"
-  choice = gets.chomp
-  # count -=1
-  if choice == "yes"
-    puts "you should watch #{documenatary}"
-  else
-    puts " Do you like comedies instead? \n yes or no?"
-    choice = gets.chomp
-    # count -=1
-    if choice == "yes"
-      puts "you should watch #{comedy}"
 
-    else
-      puts "you should watch #{drama}"
-    end
 
-  end
+
+
+  # puts "Do you like documenataries? \n yes or no is valid input"
+  # choice = gets.chomp
+  # # count -=1
+  # if choice == "yes"
+  #   puts "you should watch #{documenatary}"
+  # else
+  #   puts " Do you like comedies instead? \n yes or no?"
+  #   choice = gets.chomp
+  #   # count -=1
+  #   if choice == "yes"
+  #     puts "you should watch #{comedy}"
+  #
+  #   else
+  #     puts "you should watch #{drama}"
+  #   end
+  #
+  # end
+
+
 
 
 # def moviechoice ()
